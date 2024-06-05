2187c83879f147e09d990b82239081212b5b45b0
lauret lucas <lucas.lauret@edu.univ-fcomte.fr>
2024-06-05 23:26:46 +0200

    404

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    Error 404 :(
+  <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 80vh;">
+    <h1 style="font-size: 2em; font-weight: bold;">404</h1>
+    <p style="font-size: 1em;">Page not found :(</p>
   </div>
 </template>
